6c50ce453922221b7c9d65590782d51761d71286
reddevill007 <shivampandey2900@gmail.com>
2022-06-06 23:56:49 +0530

    ecma script

---
+++ ECMAScript.mjs
@@ -0,0 +1,8 @@
+// CommonJs = require/module.exports
+
+// EcmaScript Module = import/export
+
+// import myDefaultExports, { five, myName } from "./another.mjs";
+import * as another from "./another.mjs";
+
+console.log(another);
---
+++ another.mjs
@@ -0,0 +1,18 @@
+const five = 5;
+
+export { five };
+
+export const ten = 10;
+
+export const myName = "Saurabh";
+
+export default "shop";
+
+// named and default export type
+
+// {
+//     five: 5,
+//     ten: 10,
+//     myName: "Saurabh",
+//     default: "shop"
+// }
